5b248271d8aaa9192b3628e6a096f5ab0afccef4
paulvha <paulvha@outlook.com>
2019-02-02 17:46:36 +0100

    Add files via upload

--- examples/Example2_SetOptions_pvh/Example2_SetOptions_pvh.ino
+++ examples/Example2_SetOptions_pvh/Example2_SetOptions_pvh.ino
@@ -16,41 +16,49 @@
   Plug the sensor onto the shield
   Serial.print it out at 9600 baud to serial monitor.
 
-  
   **********************************************************************
-  * modified paulvha : august 2018
+  * Versioning:
   **********************************************************************
-  Support ESP8266-Thing
-  include option to debug driver
-  obtain SCD30 serial number
-  obtain temperature in Fahrenheit
-  **********************************************************************
-    
-  Hardware Connections:
-  If needed, attach a Qwiic Shield to your Arduino/Photon/ESP32 or other
-  Plug the device into an available Qwiic port
-  Open the serial monitor at 9600 baud to see the output
+  august 2018 / paulvha:
+    Support ESP8266-Thing
+    include option to debug driver
+    obtain SCD30 serial number
+    obtain temperature in Fahrenheit
 
-  ELSE CONNECT TO ARDUINO UNO
-  
+  January 2019 / Paulvha
+    Added SoftWire support for ESP32
+  **********************************************************************
+  CONNECT TO ARDUINO
+
   SCD30 :
     VCC to 3V3 or 5V
     GND to GND
     SCL to SCL ( Arduino UNO A4)
     SDA to SDA ( Arduino UNO A5)
 
-  ELSE CONNECT TO ESP8266-THING
+  CONNECT TO ESP8266-THING
 
-  Make sure to cut the link and have a jumper on the DTR/reset. 
+  Make sure to cut the link and have a jumper on the DTR/reset.
   Include the jumper for programming, remove before starting serial monitor
-   
-  SCD30
-    GND TO GND
-    VCC to 3V3
-    SCL to SCL
-    SDA to SDA
-   
+
+  SCD30    ESP8266
+    GND --- GND
+    VCC --- 3V3
+    SCL --- SCL
+    SDA --- SDA
+
   Given that SCD30 is using clock stretching the driver has been modified to deal with that.
+
+  CONNECT TO ESP32-THING
+
+  SCD30    ESP32
+    GND --- GND
+    VCC --- 3V3
+    SCL --- 22
+    SDA --- 21
+
+  Given that SCD30 is using clock stretching SoftWire is selected by the driver to deal with that.
+  Make sure to press the GPIO0 button for connect /upload
 
   Note: All settings (interval, altitude, etc) are saved to non-volatile memory and are
   loaded by the SCD30 at power on. There's no damage in sending that at each power on.
@@ -83,7 +91,6 @@
 //////////////// NO CHANGES BEYOND THIS POINT NEEDED /////////////////////
 //////////////////////////////////////////////////////////////////////////
 
-#include <Wire.h>
 #include "paulvha_SCD30.h"
 
 SCD30 airSensor;
@@ -94,42 +101,42 @@ void setup()
   Wire.begin();
 
   Serial.begin(9600);
-  Serial.println("SCD30 Example");
+  Serial.println("SCD30 Example 2");
 
   // set driver debug level
-  // 0 : no messages
-  // 1 : request sending and receiving
-  // 2 : request sending and receiving + show protocol errors
   airSensor.setDebug(scd_debug);
 
-  //This will cause readings to occur every two seconds
-  airSensor.begin(); 
-  
-  // Read SCD30 serial number as printed on the device 
+  //This will init setting but NOT readings
+  airSensor.begin(Wire,false);
+
+  // Read SCD30 serial number as printed on the device
   // buffer MUST be at least 7 digits (6 serial + 0x0)
-  airSensor.getSerialNumber(buf); 
+  airSensor.getSerialNumber(buf);
   Serial.print("serial number: ");
   Serial.println(buf);
 
+  //This will cause readings to occur every two seconds
+  airSensor.begin();
+
   // Change number of seconds between measurements: 2 to 1800 (30 minutes)
   // setting to 4 will cause 3 dots before reading, due to call delay(1000)
-  airSensor.setMeasurementInterval(read_interval); 
+  airSensor.setMeasurementInterval(read_interval);
 
 /* paulvha : you can set EITHER the Altitude compensation of the pressure.
- * Setting both does not make sense as both overrule each other, but it is included for demonstration 
- * 
+ * Setting both does not make sense as both overrule each other, but it is included for demonstration
+ *
  * see Sensirion_CO2_Sensors_SCD30_Interface_Description.pdf
- *    
+ *
  *    The CO2 measurement value can be compensated for ambient pressure by feeding the pressure value in mBar to the sensor.
  *    Setting the ambient pressure will overwrite previous and future settings of altitude compensation. Setting the argument to zero
- *    will deactivate the ambient pressure compensation. For setting a new ambient pressure when continuous measurement is running 
+ *    will deactivate the ambient pressure compensation. For setting a new ambient pressure when continuous measurement is running
  *    the whole command has to be written to SCD30.
- *    
+ *
  *    Setting altitude is disregarded when an ambient pressure is given to the sensor
  */
- 
+
   // My desk is ~10m above sealevel
-  airSensor.setAltitudeCompensation(10); //Set altitude of the sensor in m
+  airSensor.setAltitudeCompensation(10); //Set altitude of the sensor in meter
 
   // Pressure in Boulder
   airSensor.setAmbientPressure(pressure); //Current ambient pressure in mBar: 700 to 1200
@@ -158,4 +165,3 @@ void loop()
 
   delay(1000);
 }
-
